c180e9b700a26d5feb8a0075be35833593f32fd9
yaowun <allen0989122068@gmail.com>
2024-07-21 12:25:05 +0800

    add quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Quasar, {
+    plugins: {},
+})
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/nasa-apod/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ]
 })
