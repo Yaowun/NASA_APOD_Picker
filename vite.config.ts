1929b714bada34040ed42118903bb6f5f92d78d6
張耀文 <90306375+Yaowun@users.noreply.github.com>
2025-03-05 22:54:36 +0800

    Update vite.config.ts
    use correct url after changed repo project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { quasar } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nasa-apod/',
+  base: '/NASA_APOD_Picker/',
   plugins: [
     vue(),
 
